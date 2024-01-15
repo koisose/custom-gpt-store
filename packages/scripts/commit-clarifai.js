079a6e3d2643ce14c9305ffb525e5d20599446cf
koisose.eth <plokotokbelajar@gmail.com>
2024-01-15 13:30:11 +0000

    📝 docs(README): perbarui instruksi penggunaan auto commit
    Menghapus bagian tentang penggunaan Google API dan Clarifai GPT-4 untuk auto commit dari README. Sekarang hanya menampilkan 'ask gpt' yang menandakan penggunaan instruksi yang lebih sederhana.

--- packages/scripts/commit-clarifai.js
+++ packages/scripts/commit-clarifai.js
@@ -111,14 +111,16 @@ async function gitDiffStaged() {
   
       
       const text=await postModelOutputsPromise(diffString)
-      console.log(text.outputs[0].data.text.raw);
-      let text2=text.outputs[0].data.text.raw.replace(/\`/gi, "\\`");
+     
+      let text2=text.outputs[0].data.text.raw.replace(/```/g, '');
       let text3=text2.replace(/---/g, '')
-    //   execSync(`git reset`);
-    //   console.log(text3)
+      let text4=text3.replace(/\"/gi, "\\\"")
+      let text5=text4.replace(/\`/gi, "\\`");
+      console.log(text5)
+      // execSync(`git reset`);
       execSync(`git add -A`);
-      execSync(`printf "${text3}" | git commit -F-`);
-      execSync("git push -u origin main");
+      execSync(`printf "${text5}" | git commit -F-`);
+      execSync("git push -u origin main --force");
       process.exit();
     } catch (e) {
       console.log(e.message);
