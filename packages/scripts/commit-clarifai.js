359a973b48501cbe7058450862b542ffe3ade0ef
koisose.eth <plokotokbelajar@gmail.com>
2024-01-16 00:49:39 +0000

    🛠 fix(scripts): hapus opsi `--force` dari perintah git push
    Menghapus opsi `--force` dari perintah `git push` dalam skrip commit-clarifai untuk mencegah penimpaan riwayat commit yang tidak disengaja.

--- packages/scripts/commit-clarifai.js
+++ packages/scripts/commit-clarifai.js
@@ -120,7 +120,7 @@ async function gitDiffStaged() {
       // execSync(`git reset`);
       execSync(`git add -A`);
       execSync(`printf "${text5}" | git commit -F-`);
-      execSync("git push -u origin main --force");
+      execSync("git push -u origin main");
       process.exit();
     } catch (e) {
       console.log(e.message);
