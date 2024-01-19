9242eefbaac053c7bd24ec236fdfe6011c307bdb
koisose.eth <plokotokbelajar@gmail.com>
2024-01-19 01:39:27 +0000

    🔧 refactor(package.json): Update streamlit script and add streamlit-frontend. Refactor the streamlit script to use pnpm and introduce a new script for running the streamlit frontend specifically.
    Indonesian translation:
    
    🔧 refactor(package.json): Perbarui skrip streamlit dan tambah skrip streamlit-frontend.
    Merombak skrip streamlit untuk menggunakan pnpm dan memperkenalkan skrip baru untuk menjalankan frontend streamlit secara spesifik.

--- packages/module-chatbot/explore.py
+++ packages/module-chatbot/explore.py
@@ -1,8 +1,8 @@
 import streamlit as st
-
+from my_component import my_component
 st.set_page_config(
     page_title="Clarifai",
     page_icon="👋",
 )
-
+num_clicks = my_component("World")
 st.sidebar.write("")
