2e0c28d0d4455ad409570206b53a6e93d6770fba
koisose.eth <plokotokbelajar@gmail.com>
2024-01-19 01:42:02 +0000

    🛠️ build(scripts): Change initial commit script command. Commented out the `git add .` command and replaced it with `bash add-first-untracked.sh` to handle initial commit of untracked files more efficiently.
    Indonesian translation:
    
    🛠️ build(scripts): Ubah perintah skrip commit awal.
    Mengomentari perintah `git add .` dan menggantinya dengan `bash add-first-untracked.sh` untuk menangani commit file yang belum terlacak dengan lebih efisien.

--- packages/scripts/commit-clarifai.js
+++ packages/scripts/commit-clarifai.js
@@ -111,8 +111,8 @@ async function gitDiffStaged() {
   }
   async function run() {
     try {
-      execSync(`git add .`);
-      // execSync(`cd ../../ && bash add-first-untracked.sh`);
+      // execSync(`git add .`);
+      execSync(`cd ../../ && bash add-first-untracked.sh`);
       const diffString = await gitDiffStaged();
       if (!diffString.trim()) {
         throw { status: 5001, message: "No changes to commit" };
