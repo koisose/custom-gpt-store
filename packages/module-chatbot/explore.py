0e8a3551c463cb138588a4ecd81f36a5948ebd1b
koisose <plokotokbelajar@gmail.com>
2024-01-20 21:12:02 +0700

    🔧 refactor(module-chatbot): Comment out conditional component declaration. Commented out the conditional _RELEASE check and associated component declaration code. Simplified the component initialization to always use the build directory path.
    Indonesian translation:
    
    🔧 refactor(module-chatbot): Komentari deklarasi komponen bersyarat.
    Mengomentari pemeriksaan _RELEASE bersyarat dan kode deklarasi komponen yang terkait. Menyederhanakan inisialisasi komponen untuk selalu menggunakan jalur direktori build.

--- packages/module-chatbot/explore.py
+++ packages/module-chatbot/explore.py
@@ -13,20 +13,23 @@ with col1:
     st.button('Say hello')
     st.button('Say helddlo',key="haha")
 
-if not _RELEASE:
-    _component_func = components.declare_component(
+# if not _RELEASE:
+#     _component_func = components.declare_component(
+#
+#         "my_component",
+#
+#         url="http://localhost:3001",
+#     )
+#     _component_func()
+# else:
 
-        "my_component",
-
-        url="http://localhost:3001",
-    )
-    _component_func()
-else:
-
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "frontend/build")
-    _component_func = components.declare_component("my_component", path=build_dir)
-    _component_func()
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(parent_dir, "frontend/build")
+st.write(build_dir)
+_component_func = components.declare_component("my_component", path=build_dir)
+_component_func()
+    # parent_dir = os.path.dirname(os.path.abspath(__file__))
+    # build_dir = os.path.join(parent_dir, "frontend/build")
 
 
 
